97be86cb63213f08f043c60779fc8e4776a839ba
zablon <zablonoigo51@gmail.com>
2024-08-18 14:13:40 +0300

    Added register  view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from .utils import send_code_to_user 
+class RegisterUserView(GenericAPIView):
+    serializer_class=UserRegisterSerializer
 
-# Create your views here.
+    def post(self,request):
+        user_data=request.data
+        serializer=self.serializer_class(data=user_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user=serializer.data
+            send_code_to_user(user['email'])
+            return  Response({
+                'data':user
+            },
+            status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
