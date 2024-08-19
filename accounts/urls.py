16fb181e09a5d371e1e7b7ae180cfaba3ba4c86d
zablon <zablonoigo51@gmail.com>
2024-08-19 14:26:43 +0300

    Added token url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegisterUserView,VerifyUserEmail
-
+from .views import RegisterUserView,VerifyUserEmail,LoginUser
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 urlpatterns=[
     path("register/",RegisterUserView.as_view(),name="register"),
-    path("verify/",VerifyUserEmail.as_view(),name="verify")
+    path("verify/",VerifyUserEmail.as_view(),name="verify"),
+    path("token/", TokenObtainPairView.as_view(),name="token"),
+    path("refresh/",TokenRefreshView.as_view(),name="refresh"),
+    path("login/",LoginUser.as_view(),name="login"),
 ]
