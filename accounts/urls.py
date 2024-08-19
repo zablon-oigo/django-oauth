1968808cbb92fe052ce39a529265251881cf906b
zablon <zablonoigo51@gmail.com>
2024-08-19 11:56:36 +0300

    added email verfication url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView,VerifyUserEmail
 
 urlpatterns=[
-    path("register/",RegisterUserView.as_view(),name="register")
+    path("register/",RegisterUserView.as_view(),name="register"),
+    path("verify/",VerifyUserEmail.as_view(),name="verify")
 ]
