a77a2b9554e9ba6ef8220e7e3c6459174331a34e
zablon <zablonoigo51@gmail.com>
2024-08-20 15:56:09 +0300

    added password reset path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterUserView,VerifyUserEmail,LoginUser
+from .views import RegisterUserView,VerifyUserEmail,LoginUser,PasswordResetConfirm,SetNewPassword,PasswordResetRequestView
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 urlpatterns=[
     path("register/",RegisterUserView.as_view(),name="register"),
@@ -7,4 +7,8 @@ urlpatterns=[
     path("token/", TokenObtainPairView.as_view(),name="token"),
     path("refresh/",TokenRefreshView.as_view(),name="refresh"),
     path("login/",LoginUser.as_view(),name="login"),
+    path("password-reset/",PasswordResetRequestView.as_view(),name="password-reset"),
+    path("password-reset-confirm/<uidb64>/<token>/",PasswordResetConfirm.as_view(), name="password-reset-confirm"),
+    path("set-new-password/",SetNewPassword.as_view(),name="set-password")
+
 ]
