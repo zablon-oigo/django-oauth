e57d574c80549ad30e159602a11be6759c721cc5
zablon <zablonoigo51@gmail.com>
2024-08-21 16:08:52 +0300

    Added logout url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterUserView,VerifyUserEmail,LoginUser,PasswordResetConfirm,SetNewPassword,PasswordResetRequestView
+from .views import RegisterUserView,VerifyUserEmail,LoginUser,PasswordResetConfirm,SetNewPassword,PasswordResetRequestView,LogoutUserView
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 urlpatterns=[
     path("register/",RegisterUserView.as_view(),name="register"),
@@ -9,6 +9,7 @@ urlpatterns=[
     path("login/",LoginUser.as_view(),name="login"),
     path("password-reset/",PasswordResetRequestView.as_view(),name="password-reset"),
     path("password-reset-confirm/<uidb64>/<token>/",PasswordResetConfirm.as_view(), name="password-reset-confirm"),
-    path("set-new-password/",SetNewPassword.as_view(),name="set-password")
+    path("set-new-password/",SetNewPassword.as_view(),name="set-password"),
+    path("logout/",LogoutUserView.as_view(),name="logout")
 
 ]
