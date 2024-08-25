bd683ca9a0021fe816c6122b33ea5106d413f420
zablon <zablonoigo51@gmail.com>
2024-08-25 19:13:53 +0300

    Added cors origin

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "accounts",
-    "corsheader"
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -136,9 +137,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 CORS_ALLOW_ALL_ORIGINS=True
-CSRF_TRUSTED_ORIGINS=[
-    'http://127.0.0.1:5173/',
-    'http://localhost:5173/',
+CORS_ALLOWED_ORIGINS=[
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
