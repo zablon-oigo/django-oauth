4a19e2bc568cade199a2e6d136fbd1fc7ed4be33
zablon <zablonoigo51@gmail.com>
2024-08-26 19:10:02 +0300

    Added verify email container

--- frontend/src/components/VerifyEmail.jsx
+++ frontend/src/components/VerifyEmail.jsx
@@ -1,9 +1,9 @@
 export default function VerifyEmail() {
   return (
     <section className="">
-      <form action="" className="">
+      <form action="" className="flex items-center justify-center">
         <div className="">
-          <label htmlFor="" className="">Enter your OTP code:</label>
+          <label htmlFor="" className="text-2xl font-semibold ">Enter your OTP code:</label>
           <input type="text" className="" />
         </div>
       </form>
