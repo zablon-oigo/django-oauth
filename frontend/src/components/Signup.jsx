065a56d76d9127b69f9d24ffa272d3acd8c1b7cb
zablon <zablonoigo51@gmail.com>
2024-08-26 19:04:46 +0300

    styling user form

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -42,74 +42,74 @@ export default function Signup() {
   };
 
   return (
-    <section className="max-w-3xl mx-auto my-40 bg-gray-100">
+    <section className="max-w-3xl px-20 py-10 mx-auto my-40 bg-gray-100">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center max-w-sm px-10 py-6 mx-auto bg-white ">
       <div className="">
-        <h2 className='text-3xl font-light text-center first-letter:capitalize'>create account</h2>
+        <h2 className='text-2xl font-semibold text-center capitalize text-slate-800'>create account</h2>
       </div>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center px-10 py-6 bg-white ">
         {error && <p className="">{error}</p>}
         
         <div className="flex flex-col mt-2">
-          <label htmlFor="email" className="mb-2 text-gray-800">Email Address</label>
+          <label htmlFor="email" className="text-gray-800 text-md">Email Address</label>
           <input 
             type="email" 
             id="email"
             name="email"
-            className="px-6 py-3 rounded-lg " 
+            className="w-full px-6 py-3 border-2 border-gray-200 rounded-lg " 
             value={formData.email} 
             onChange={handleOnChange}
           />
         </div>
         
         <div className="flex flex-col mt-2">
-          <label htmlFor="first_name" className="mb-2 text-gray-800">First Name</label>
+          <label htmlFor="first_name" className="text-gray-800 text-md">First Name</label>
           <input 
             type="text" 
             id="first_name"
             name="first_name"
-            className="px-6 py-3 rounded-lg" 
+            className="w-full px-6 py-3 border-2 border-gray-200 rounded-lg" 
             value={formData.first_name} 
             onChange={handleOnChange}
           />
         </div>
         
         <div className="flex flex-col mt-2">
-          <label htmlFor="last_name" className="mb-2 text-gray-800">Last Name</label>
+          <label htmlFor="last_name" className="text-gray-800 text-md">Last Name</label>
           <input 
             type="text" 
             id="last_name"
             name="last_name"
-            className="px-6 py-3 rounded-lg" 
+            className="w-full px-6 py-3 border-2 border-gray-200 rounded-lg" 
             value={formData.last_name} 
             onChange={handleOnChange}
           />
         </div>
         
         <div className="flex flex-col mt-2">
-          <label htmlFor="password" className="mb-2 text-gray-800">Password</label>
+          <label htmlFor="password" className="text-gray-800 text-md">Password</label>
           <input 
             type="password" 
             id="password"
             name="password"
-            className="px-6 py-3 rounded-lg" 
+            className="w-full px-6 py-3 border-2 border-gray-200 rounded-lg" 
             value={formData.password} 
             onChange={handleOnChange}
           />
         </div>
         
         <div className="flex flex-col mt-2">
-          <label htmlFor="password2" className="mb-2 text-gray-800">Repeat Password</label>
+          <label htmlFor="password2" className="text-gray-800 text-md">Repeat Password</label>
           <input 
             type="password" 
             id="password2"
             name="password2"
-            className="px-6 py-3 rounded-lg" 
+            className="w-full px-6 py-3 border-2 border-gray-200 rounded-lg" 
             value={formData.password2} 
             onChange={handleOnChange}
           />
         </div>
         
-        <input type="submit" value="Submit" className="w-1/3 px-6 py-3 mx-auto mt-2 text-white bg-teal-700 rounded-lg" />
+        <input type="submit" value="Submit" className="w-[80%] px-6 py-3 mx-auto mt-2 text-white bg-teal-700 rounded-lg" />
       </form>
       
       <h3 className="">or</h3>
