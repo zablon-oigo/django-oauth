945d45250d92c6c98556c4fa4982546877908662
zablon <zablonoigo51@gmail.com>
2024-08-27 18:59:48 +0300

    Added funtion to handle the state

--- frontend/src/components/VerifyEmail.jsx
+++ frontend/src/components/VerifyEmail.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react"
 export default function VerifyEmail() {
+  const[otp,setOTP]=useState("")
   return (
     <section className="max-w-3xl px-10 py-10 mx-auto bg-gray-100 my-60 rounded-xl">
       <form action="" className="max-w-sm mx-auto">
         <div className="flex flex-col">
           <label htmlFor="" className="mb-2 text-2xl font-semibold ">Enter your OTP code:</label>
-          <input type="text" className="px-6 py-3 border-2 border-gray-600 rounded-lg " />
+          <input type="text" value={otp} onChange={(e)=>setOTP(e.target.value)} className="px-6 py-3 border-2 border-gray-600 rounded-lg " />
         </div>
         <input type="submit" value="submit" className="w-full px-6 py-3 mt-2 text-white bg-teal-700 rounded-lg" />
       </form>
