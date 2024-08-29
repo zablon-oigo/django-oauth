84d197a484a40e055f42d6f2e1569502923750ea
zablon <zablonoigo51@gmail.com>
2024-08-29 13:28:24 +0300

    Added user profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Profile() {
+   const navigate=useNavigate()
+   const user=JSON.parse(localStorage.getItem('user'))
+   const jwt_access=localStorage.getItem('access')
+   useEffect(()=>{
+    if(jwt_access === null && !user){
+      navigate("/login")
+    }
+   },[])
+
   return (
-    <div>Profile</div>
-  )
+    <section className="max-w-md p-6 mx-auto bg-white rounded-lg shadow-md">
+      <div className="flex flex-col items-center">
+        <h2 className="mb-2 text-3xl font-semibold">
+          Hello, {user && user.full_name}
+        </h2>
+        <p className="mb-4 text-gray-600">Welcome back to your profile!</p>
+        <button 
+          className="px-4 py-2 text-white transition duration-300 bg-blue-500 rounded-lg shadow-md hover:bg-blue-600"
+        >
+          Logout
+        </button>
+      </div>
+    </section>
+  );
 }
